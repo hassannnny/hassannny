eaf0e24588ee7972e1c534abeedee5d57aa26899
hassannnny <lhjaved@neiu.edu>
2025-08-26 16:01:44 -0500

    name and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
+import React from 'react'
+import { Container, Row, Col, Nav, Navbar, NavbarBrand } from 'reactstrap'
+import miLogo from './assets/miLogo.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container style={{height: '100vh', width: '100vw', color: 'white'}}>
+      <Row style={{display: "inline-flex"}}>
+        <Col style={{width: '50vw'}}><h1 style={{fontSize: '15vh', paddingLeft: '10vw', paddingTop: '15vh'}}>حسنننني</h1></Col>
+        <Col style={{width: '50vw', paddingTop: '0vh'}}><img src={miLogo} height={700} width={700} /></Col>
+      </Row>
+    </Container>
   )
 }
 
